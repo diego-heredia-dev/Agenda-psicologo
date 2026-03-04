d261c5b062bf25f8c9d59b0cc63d11da29bdb6bd
Diego <diego88aht@gmail.com>
2026-03-03 23:13:47 -0400

    Dias clickeables para crear nueva cita y calendario con formatode mes - semana - dia

--- main.js
+++ main.js
@@ -7,11 +7,35 @@ document.addEventListener('DOMContentLoaded', function () {
     calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'timeGridWeek',
         locale: 'es',
-        selectable: true,
+
+        headerToolbar: {
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay"
+        },
+        
+        selectable: false,
         slotMinTime: "08:00:00",
         slotMaxTime: "20:00:00",
         allDaySlot: false,
+        height: "auto",
+        
+        //abre formulario de nueva cita
+        dateClick: function(info) {
+            const clickedDate = info.date;
 
+            const year = clickedDate.getFullYear();
+            const month = String(clickedDate.getMonth() + 1).padStart(2, '0');
+            const day = String(clickedDate.getDate()).padStart(2, '0');
+
+            const dateOnly = `${year}-${month}-${day}`;
+
+            document.getElementById("appointment-date").value = dateOnly;
+
+            openForm();
+        },
+
+        //muestra info de la cita cuando se da click sobre ella
         eventClick: function(info) {
             const event = info.event;
 
@@ -52,23 +76,27 @@ function saveAppointment() {
     const name = document.getElementById("patient-name").value;
     const lastname = document.getElementById("patient-lastname").value;
     const description = document.getElementById("patient-description").value;
+    const date = document.getElementById("appointment-date").value;
     const start = document.getElementById("appointment-start").value;
     const end = document.getElementById("appointment-end").value;
 
-    if (!name || !lastname || !start || !end) {
+    if (!name || !lastname || !date || !start || !end) {
         alert("Por favor completar todos los campos obligatorios");
         return;
     }
 
-    if (new Date(start) >= new Date(end)) {
+    const startDateTime = new Date(date + "T" + start);
+    const endDateTime = new Date(date + "T" + end);
+
+    if (startDateTime >= endDateTime) {
         alert("La feha de inicio debe ser menor que la fecha de fin");
         return;
     }
 
     calendar.addEvent({
         title: name + " " + lastname,
-        start: start,
-        end: end,
+        start: startDateTime,
+        end: endDateTime,
         extendedProps: {
             description: description
         }
